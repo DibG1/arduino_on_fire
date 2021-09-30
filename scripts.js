ce182da79409050cb95cce3e46fc7d1c5f114ce4
DibG1 <78297958+DibG1@users.noreply.github.com>
2021-09-30 15:55:24 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,37 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var config = {
+  apiKey: "AIzaSyAE5kbcoKdl1RbenvlpVtp_b1EzvQqxTYQ",
+  authDomain: "ledstats-dfc7c.firebaseapp.com",
+  databaseURL: "https://ledstats-dfc7c-default-rtdb.firebaseio.com",
+  projectId: "ledstats-dfc7c",
+  storageBucket: "ledstats-dfc7c.appspot.com",
+  messagingSenderId: "122796939618",
+
+
+};
+firebase.initializeApp(config);
+
+$(document).ready(function(){
+  var database=firebase.database();
+  var ledpos;
+
+  database.ref().on("value", function(snap){
+    ledpos=snap.val().leeds;
+    if(ledpos==1){
+      $(".ledpos").text("LED is ON");
+    }else{
+        $(".ledpos").text("LED is OFF");
+    }
+  });
+  $(".LEDbutton").click(function(){
+  var firebaseRef=firebase.database().ref().child("ledpos");
+
+  if(ledpos==1){
+    firebaseRef.set(0);
+    ledpos=0;
+  }  else{
+    firebaseRef.set(1);
+    ledpos=1;
+  }
+  });
+});
